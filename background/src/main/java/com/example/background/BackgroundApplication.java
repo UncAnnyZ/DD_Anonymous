0dbf5f8cb30987585a4d9546f07c0d70a9a5ec39
UncAnnyZ <49876144+UncAnnyZ@users.noreply.github.com>
2021-02-04 14:24:43 +0800

    后台架构更新
    后台架构更新

---
+++ background/src/main/java/com/example/background/BackgroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.background;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundApplication.class, args);
+    }
+
+}
